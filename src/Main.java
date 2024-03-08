87d75d35f67cb241315b2e01d153ab85a927419c
Your Name <you@example.com>
2024-03-08 11:45:23 +0200

    3 praktyche

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@
 public class Main {
     public static void main(String[] args) {
 //        Завдання 1"
-        int a = 18;
-        int b = 15;
-
-        float dilennya = (float) a/b;
-        System.out.println(dilennya);
-
-        int ostacha = a % b;
-        System.out.println(ostacha);
+//        int a = 18;
+//        int b = 15;
+//
+//        float dilennya = (float) a/b;
+//        System.out.println(dilennya);
+//
+//        int ostacha = a % b;
+//        System.out.println(ostacha);
 
 //      Завдання 2
 //        int a = 29;
@@ -19,5 +19,13 @@ public class Main {
 //        int secondNumber = a % 10;
 //        int finishNumber = firstNumber + secondNumber;
 //        System.out.println(finishNumber);
+
+        double a = 5.6;
+        double b = 0.5;
+        int okruhlennya =(int) (a + b);
+        System.out.println(okruhlennya);
     }
+
+
+
 }
