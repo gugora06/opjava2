4385290f0f7eb9ea9b9872cca9236fbb1937d4a5
Your Name <you@example.com>
2024-03-08 11:28:20 +0200

    gigigig

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        Завдання 1"
+        int a = 18;
+        int b = 15;
+
+        float dilennya = (float) a/b;
+        System.out.println(dilennya);
+
+        int ostacha = a % b;
+        System.out.println(ostacha);
+
+//      Завдання 2
+//        int a = 29;
+//
+//        int firstNumber = a / 10;
+//        int secondNumber = a % 10;
+//        int finishNumber = firstNumber + secondNumber;
+//        System.out.println(finishNumber);
+    }
+}
